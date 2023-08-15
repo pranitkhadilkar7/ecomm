866f23c8591ec5055d86727f02394849b103d7e3
Pranit Khadilkar <pranit@emtech.com>
2023-08-15 12:11:54 +0530

    added html wepack plugin to add js files created in the dist folder into the scripts tag of the html file inside public folder

--- products/webpack.config.js
+++ products/webpack.config.js
@@ -1,6 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     mode: 'development',
     devServer: {
         port: 8085
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
 };
